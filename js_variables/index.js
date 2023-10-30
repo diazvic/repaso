1a55ac67704e74719f605144c464f1b9d604995c
Victoria Diaz <diazmariavictoria94@gmail.com>
2023-10-30 18:00:52 +0100

    Diferencias entre numeros y strings

--- js_variables/index.js
+++ js_variables/index.js
@@ -2,9 +2,15 @@
 let nombre; //declarar una variable
 
 nombre = "Capu"; // asignarle valor a una variable
-alert(nombre);
+// alert(nombre);
 console.log(nombre);
 let otroNombre = "Euge"; // declaracion y asignacion en el mismo lugar
 // alert(otroNombre);
 console.log(otroNombre);
 // string ---> lo pongo entre comillas
+
+// let numero = 1;
+
+// console.log(numero);
+// let numeroUno = "1";
+// console.log(numeroUno);
