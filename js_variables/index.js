d85ddb4c458bf2141c3df9af08efe695d6c4402d
Victoria Diaz <diazmariavictoria94@gmail.com>
2023-10-30 15:20:04 +0100

    Declaro variables

--- js_variables/index.js
+++ js_variables/index.js
@@ -1,1 +1,6 @@
-alert("hola");
+// alert("hola");
+let nombre; //declarar una variable
+nombre = "Capu"; // asignarle valor a una variable
+alert(nombre);
+let otroNombre = "Euge"; // declaracion y asignacion en el mismo lugar
+// alert(otroNombre);
