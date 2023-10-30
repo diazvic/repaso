1f7baa7ca58b78a53129e1c626ecf37a2eeb3fb0
Victoria Diaz <diazmariavictoria94@gmail.com>
2023-10-30 15:29:29 +0100

    Prueba de Consola

--- js_variables/index.js
+++ js_variables/index.js
@@ -1,6 +1,10 @@
 // alert("hola");
 let nombre; //declarar una variable
+
 nombre = "Capu"; // asignarle valor a una variable
 alert(nombre);
+console.log(nombre);
 let otroNombre = "Euge"; // declaracion y asignacion en el mismo lugar
 // alert(otroNombre);
+console.log(otroNombre);
+// string ---> lo pongo entre comillas
