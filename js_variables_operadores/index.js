781160f1c28ad84d3941f2877fddcaa9517328df
Victoria Diaz <diazmariavictoria94@gmail.com>
2023-10-31 15:29:03 +0100

    Resolviendo ejercicios

--- js_variables_operadores/index.js
+++ js_variables_operadores/index.js
@@ -1,7 +1,32 @@
 console.log("hola");
 
-let nombre = "Vic";
-console.log(nombre);
-let mascota = prompt("Escribe el nombre de tu mascota");
-console.log(mascota);
-alert("Hola " + mascota);
+// let nombre = "Vic";
+// console.log(nombre);
+// let mascota = prompt("Escribe el nombre de tu mascota");
+// console.log(mascota);
+// alert("Hola " + mascota);
+
+// los prompt retornan siempre un string
+
+// typeof mascota;
+// console.log(typeof mascota);
+
+// let numero = 33;
+// console.log(typeof numero);
+
+// Ejercicio
+// let nombre = prompt("Cual es tu nombre ?");
+// console.log(nombre);
+// let apellido = prompt("Cual es tu apellido?");
+// console.log(apellido);
+
+// let nombreCompleto = `Hola ${nombre} ${apellido}, bienvenida a Ada`;
+// console.log(nombreCompleto);
+//Ejercicio
+
+// let gusto1 = prompt("Cual es el primer gusto?");
+// let gusto2 = prompt("Cual es el segundo gusto?");
+// let gusto3 = prompt("Cual es el tercer gusto?");
+
+// let gustoCompleto = `Aqui tiene su helado de ${gusto1}, ${gusto2} y ${gusto3} `;
+// console.log(gustoCompleto);
