a058362e5d8bf6c0560a49b2080051c226a2b82e
Victoria Diaz <diazmariavictoria94@gmail.com>
2023-11-02 16:41:50 +0100

    Ejercicios

---
+++ funciones/index.js
@@ -0,0 +1,19 @@
+// console.log("hola");
+
+// let funcionPrueba = () => {
+// 	alert("Hola");
+// };
+// funcionPrueba();
+
+const gatosPrueba = (gatos) => {
+	console.log(`Hola ${gatos}`);
+};
+gatosPrueba("Java");
+gatosPrueba("Capu");
+
+//funcion suma
+const sumar = (numero1, numero2) => {
+	return console.log(numero1 + numero2);
+};
+sumar(2, 6);
+sumar(3, 3);
