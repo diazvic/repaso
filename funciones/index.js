8454bab5c0850bb81e634050d2cb933d641c8758
Victoria Diaz <diazmariavictoria94@gmail.com>
2023-11-06 19:59:46 +0100

    Ejercicios

--- funciones/index.js
+++ funciones/index.js
@@ -5,15 +5,28 @@
 // };
 // funcionPrueba();
 
-const gatosPrueba = (gatos) => {
-	console.log(`Hola ${gatos}`);
-};
-gatosPrueba("Java");
-gatosPrueba("Capu");
+// const gatosPrueba = (gatos) => {
+// 	console.log(`Hola ${gatos}`);
+// };
+// gatosPrueba("Java");
+// gatosPrueba("Capu");
 
 //funcion suma
-const sumar = (numero1, numero2) => {
-	return console.log(numero1 + numero2);
-};
-sumar(2, 6);
-sumar(3, 3);
+// const sumar = (numero1, numero2) => {
+// 	let suma = numero1 + numero2;
+// 	return suma;
+// };
+
+// let resultado = sumar(2, 7);
+
+// console.log(resultado);
+// sumar(2, 6);
+// sumar(3, 3);
+
+//Defini una funcion gritar q reciba como argumento un string y devuelva el mismo string con un signo de exclamacion al principio y al final del mismo
+// let gritar = (saludo) => {
+// 	return `¡${saludo}!`;
+// };
+
+// console.log(gritar("Hola"));
+// console.log(gritar("Prueba"));
