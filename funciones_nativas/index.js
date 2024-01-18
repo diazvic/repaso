0b490d8638688e76ffc4b60465f517bc8951fb62
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-01-18 21:02:22 +0100

    Funciones Nativas

---
+++ funciones_nativas/index.js
@@ -0,0 +1,22 @@
+const nombre = "Java";
+
+nombre.length;
+
+console.log(nombre.length);
+
+const nombreMayus = nombre.toUpperCase();
+console.log(nombreMayus);
+
+console.log(nombre.charAt(0));
+console.log(nombre.charAt(3));
+
+console.log(nombre.slice(1, 4));
+
+const frase = "Aguante Capu";
+
+console.log(frase.replace("C", "J"));
+
+const fraseNueva = "Hola estoy probando";
+// /a/ expresion regular
+console.log(fraseNueva.replace(/a/gi, "i"));
+/[ae]/;
