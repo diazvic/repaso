b30a9b2e3596fb999074935e3f29782a86a581b2
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-01-18 19:07:33 +0100

    Condicionales

---
+++ condicionales/index.js
@@ -0,0 +1,55 @@
+// console.log("hola");
+
+// const colorSemaforo = "rojo";
+
+// if (colorSemaforo === "verde") {
+// 	alert("podes avanzar");
+// } else {
+// 	alert("no podes avanzar");
+// }
+
+// const esPositivoONegativo = (numero) => {
+// 	if (numero > 0) {
+// 		return "positivo";
+// 	} else {
+// 		return "negativo";
+// 	}
+// };
+
+//  defini la funcion es esPositivoONegativo que reciba como argumento un numero y devuelva
+//  el string positivo si el numero es positivo, o el string negativo, si
+//  el numero es negativo
+// const resultado = esPositivoONegativo(3);
+
+// console.log(resultado);
+//  defini una funcion avanzarSemaforo que reciba como argumento un string
+//  colorActual y devuelva un string con el sig color del semaforo, siguiendo el orden:
+//  verde - amarillo- rojo - verde
+
+// const avanzarSemaforo = (colorActual) => {
+// 	if (colorActual === "verde") {
+// 		return "amarillo";
+// 	} else if (colorActual === "amarillo") {
+// 		return "rojo";
+// 	} else if (colorActual === "rojo") return "verde";
+// };
+
+// semaforo = avanzarSemaforo("rojo");
+// console.log(semaforo);
+
+// Defini una funcion esConsonante que reciba como argumento un string letra y nos indique
+//si letra es una consonante
+
+// const esConsonante = (letra) => {
+// 	if (
+// 		letra === "a" ||
+// 		letra === "e" ||
+// 		letra === "i" ||
+// 		letra === "o" ||
+// 		letra === "u"
+// 	)
+// 		return false;
+// 	else return true;
+// };
+// resultado = esConsonante("o");
+// console.log(resultado);
