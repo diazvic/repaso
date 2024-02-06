6fbbea304cf0482b232361663059345aca499bb8
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-02-06 21:36:19 +0100

    Ejercitacion cambiando de estado

--- estado_en_react/src/App.js
+++ estado_en_react/src/App.js
@@ -1,9 +1,10 @@
 //Estado - Eventos
-
+import "./App.css";
 import { useState } from "react";
 
 const App = () => {
 	const [contador, setContador] = useState(0);
+	const [color, setColor] = useState("rojo");
 
 	const handleClickBotonMas = () => {
 		console.log("click");
@@ -14,11 +15,27 @@ const App = () => {
 		console.log("click");
 		setContador(contador - 1);
 	};
+	// const handleClickCambiarColor = () => {
+	// 	setColor("azul");
+	// };
+
+	const handleClickCambiarColorARojo = () => {
+		setColor("rojo");
+	};
+
+	const handleClickCambiarColor = () => {
+		setColor(color === "rojo" ? "azul" : "rojo");
+	};
 	return (
-		<div>
+		<div className={`container ${color}`}>
 			<p>Contador: {contador}</p>
 			<button onClick={handleClickBotonMas}>+1</button>
 			<button onClick={handleClickBotonMenos}>-1</button>
+			<button onClick={handleClickCambiarColor}>Poner el fondo azul</button>
+			<button onClick={handleClickCambiarColorARojo}>
+				Poner el fondo rojo
+			</button>
+			<button onClick={handleClickCambiarColor}>{color}</button>
 		</div>
 	);
 };
